2971d70c3f41c14949f289ec1d78a7075c39bc57
LucasMader <83018808+LucasMader@users.noreply.github.com>
2021-04-25 18:30:45 +0200

    Add files via upload

---
+++ kalorien-tracker/src/test/java/htw/berlin/kalorientracker/KalorienTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.kalorientracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalorienTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
